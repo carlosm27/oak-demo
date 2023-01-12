16b24da7662f4f7951c83be16e3eb5d87411f9e4
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-12 14:36:21 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
-const app = new Application();
-const PORT = 8000;
+const ROOT_DIR = "./file", ROOT_DIR_PATH = "/file";
 
-app.use((ctx) => {
-  ctx.response.body = "Hello OAK!";
-  console.log(`${ctx.request.method} ${ctx.request.url}`);
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello World"
 });
 
-console.log(`Listening in Port: ${PORT}`);
-await app.listen({ port: PORT});
 
+
+const app = new Application();
+
+app.use(async (ctx, next) => {
+  if (!ctx.request.url.pathname.startsWith(ROOT_DIR_PATH)) {
+    next();
+    return;
+  }
+  const filePath = ctx.request.url.pathname.replace(ROOT_DIR_PATH, "");
+  await send(ctx, filePath, {
+    root: ROOT_DIR,
+  });
+});
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
