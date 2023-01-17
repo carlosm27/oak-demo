e559ac9af77582d973ad5b1797d06047385f2a20
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-16 22:07:19 -0400

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,6 +1,6 @@
 import { Database, SQLite3Connector } from 'https://deno.land/x/denodb@v1.2.0/mod.ts';
 
-import {Links} from './model/Links.ts';
+import {Person} from './model/Person.ts';
 
 
 const connector = new SQLite3Connector({
@@ -9,6 +9,6 @@ const connector = new SQLite3Connector({
 
 export const db = new Database(connector);
 
-db.link([Links])
+db.link([Person])
 
 export default db
