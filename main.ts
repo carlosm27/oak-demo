3f5234cd5d91e18ee423af502ca619069e517b2f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-01-19 14:24:06 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Application,Router, send } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import logger from "https://deno.land/x/oak_logger/mod.ts";
 import {db } from "./db.ts"
-import {AllPersons, GetPerson, CreatePerson, UpdatedPerson, DeletedPerson} from "./controllers/Controllers.ts"
+import {AllPersons, GetPerson, CreatePerson, UpdatePerson, DeletePerson} from "./controllers/Controllers.ts"
 
 const ROOT_DIR = "./file";
 
@@ -14,8 +14,8 @@ const router = new Router();
 router.get("/persons", AllPersons)
 router.get("/person/:id", GetPerson);
 router.post("/person", CreatePerson);
-router.put("/person/:id", UpdatedPerson);
-router.delete("/person/:id", DeletedPerson);
+router.put("/person/:id", UpdatePerson);
+router.delete("/person/:id", DeletePerson);
 
 app.use(logger.logger);
 app.use(router.routes());
